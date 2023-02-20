f1e8a61f5d5912de4a30b7e2a00ea091cc225201
AdamLebo <alieberman10@aol.com>
2023-02-20 09:22:55 -0500

    added manager and start of app

--- index.js
+++ index.js
@@ -16,3 +16,30 @@ const idArray = [];
 console.log(
     '\nUse `npm run reset` to reset the dist/ folder\n'
   );
+
+  function runApp () {
+
+    function createTeam () {
+      inquirer.prompt([{
+        type: "list",
+        message: "What type of employee would you like to add to your team?",
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+      }]).then(function (userInput) {
+        switch(userInput.addEmployeePrompt) {
+          case "Manager":
+            addManager();
+            break;
+          case "Engineer":
+            addEngineer();
+            break;
+          case "Intern":
+            addIntern();
+            break;
+  
+          default:
+            htmlBuilder();
+        }
+      })
+    }
+}
