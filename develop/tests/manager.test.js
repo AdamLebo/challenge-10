880410ddf235295d7134433eaffcb664d00d31df
AdamLebo <alieberman10@aol.com>
2023-02-27 16:27:24 -0500

    added office number test to manager

--- develop/tests/manager.test.js
+++ develop/tests/manager.test.js
@@ -8,20 +8,26 @@ describe("Manager subclass", () => {
   });
 
   describe("getId", () => {
-    it("returns 1", () => {
-      expect(new Manager("Jeff", "4", "Jeff@gmail.com").getId()).toBe("4");
+    it("returns 4", () => {
+      expect(new Manager("Jeff", "4", "Jeff@gmail.com", "7").getId()).toBe("4");
     });
   });
 
   describe("getEmail", () => {
-    it("returns bob@bob.com", () => {
-      expect(new Manager("Jeff", "4", "Jeff@gmail.com").getEmail()).toBe("Jeff@gmail.com");
+    it("returns Jeff@gmail.com", () => {
+      expect(new Manager("Jeff", "4", "Jeff@gmail.com", "7").getEmail()).toBe("Jeff@gmail.com");
     });
   });
 
   describe("getRole", () => {
     it("returns Manager", () => {
-      expect(new Manager("Jeff", "4", "Jeff@gmail.com").getRole()).toBe("Manager");
+      expect(new Manager("Jeff", "4", "Jeff@gmail.com", "7").getRole()).toBe("Manager");
+    });
+  });
+
+  describe("getOfficeNumber", () => {
+    it("returns 7", () => {
+      expect(new Manager("Jeff", "4", "Jeff@gmail.com", "7").getOfficeNumber()).toBe("7");
     });
   });
 });
