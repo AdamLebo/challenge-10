cbb5c7452cda1faf1de68770579cc9bb2690a35f
AdamLebo <alieberman10@aol.com>
2023-02-27 16:08:38 -0500

    added employee test

---
+++ develop/tests/employee.test.js
@@ -0,0 +1,27 @@
+const Employee = require("../workers/employee");
+
+describe("Employee class", () => {
+  describe("getName", () => {
+    it("returns Bob", () => {
+      expect(new Employee("John", "1", "John@gmail").getName()).toBe("John");
+    });
+  });
+
+  describe("getId", () => {
+    it("returns 1", () => {
+      expect(new Employee("John", "1", "John@gmail.com").getId()).toBe("1");
+    });
+  });
+
+  describe("getEmail", () => {
+    it("returns bob@bob.com", () => {
+      expect(new Employee("John", "1", "John@gmail.com").getEmail()).toBe("John@gmail.com");
+    });
+  });
+
+  describe("getRole", () => {
+    it("returns Employee", () => {
+      expect(new Employee("John", "1", "John@gmail.com").getRole()).toBe("Employee");
+    });
+  });
+});
